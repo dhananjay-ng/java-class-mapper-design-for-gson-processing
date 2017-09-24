aa79f52d85ef79316bbbd32930d84aa60cda012e
I344422 <I344422@INLN56580002A.apj.global.corp.sap>
2017-09-25 00:21:25 +0530

    dsc

--- src/student/rest/RestTest.java
+++ src/student/rest/RestTest.java
@@ -1,6 +1,5 @@
 package student.rest;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -10,31 +9,36 @@ public class RestTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Properties prop = new Properties();
-		InputStream input = null;
+    	InputStream input = null;
 
-		try {
+    	try {
 
-			input = new FileInputStream("config.properties");
+    		String filename = "config.properties";
+    		input = RestTest.class.getClassLoader().getResourceAsStream(filename);
+    		if(input==null){
+    	            System.out.println("Sorry, unable to find " + filename);
+    		    return;
+    		}
 
-			// load a properties file
-			prop.load(input);
+    		//load a properties file from class path, inside static method
+    		prop.load(input);
 
-			// get the property value and print it out
-			System.out.println(prop.getProperty("students"));
-			System.out.println(prop.getProperty("users"));
+                //get the property value and print it out
+                System.out.println(prop.getProperty("database"));
+    	        System.out.println(prop.getProperty("dbuser"));
+    	        System.out.println(prop.getProperty("dbpassword"));
 
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+    	} catch (IOException ex) {
+    		ex.printStackTrace();
+        } finally{
+        	if(input!=null){
+        		try {
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-		}
-
+        	}
+        }
 
 
 	}
