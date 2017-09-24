83c9e26cb8439def9c6c1543ae7282be59db19c9
I344422 <I344422@INLN56580002A.apj.global.corp.sap>
2017-09-25 00:14:48 +0530

    sdds

---
+++ src/student/rest/Link.java
@@ -0,0 +1,30 @@
+package student.rest;
+
+import java.util.List;
+
+public class Link {
+	private transient  String link;
+	private  transient String rel;
+	private List<Link> links;
+	
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public String getRel() {
+		return rel;
+	}
+	public void setRel(String rel) {
+		this.rel = rel;
+	}
+	
+
+}
---
+++ src/student/rest/RestTest.java
@@ -0,0 +1,42 @@
+package student.rest;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class RestTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			System.out.println(prop.getProperty("students"));
+			System.out.println(prop.getProperty("users"));
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+
+
+	}
+
+}
